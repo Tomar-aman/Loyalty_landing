e8728525655825988ad4d4e0abe3ccaa313251a2
nsbhadawar4 <nsbhadawar4@gmail.com>
2026-01-21 08:59:16 +0000

    update privacy-policy

--- app/privacy-policy/page.tsx
+++ app/privacy-policy/page.tsx
@@ -9,48 +9,48 @@ const privacyPolicyData = [
   {
     title: "2. Information We Collect",
     description:
-      "We collect personal details such as name, email, phone number, and payment information, as well as non-personal data like browser type and usage activity.",
+      "We collect personal details such as name, email, phone number, as well as non-personal data like browser type and usage activity.",
   },
-  {
-    title: "3. How We Use Your Information",
-    description:
-      "Your information is used to provide services, process transactions, improve user experience, communicate updates, and maintain security.",
-  },
-  {
-    title: "4. Cookies & Tracking Technologies",
-    description:
-      "We use cookies and similar technologies to enhance functionality, analyze traffic, and personalize content.",
-  },
-  {
-    title: "5. Data Sharing",
-    description:
-      "We do not sell your personal data. Information may be shared with trusted partners or service providers strictly for operational purposes.",
-  },
-  {
-    title: "6. Data Security",
-    description:
-      "We implement industry-standard security measures to protect your data from unauthorized access, misuse, or disclosure.",
-  },
-  {
-    title: "7. Data Retention",
-    description:
-      "We retain personal information only as long as necessary to fulfill the purposes outlined in this policy or as required by law.",
-  },
-  {
-    title: "8. User Rights",
-    description:
-      "You have the right to access, update, correct, or delete your personal data and to opt out of marketing communications.",
-  },
-  {
-    title: "9. Third-Party Links",
-    description:
-      "Our platform may contain links to third-party websites. We are not responsible for their privacy practices or content.",
-  },
-  {
-    title: "10. Policy Updates",
-    description:
-      "We may update this Privacy Policy from time to time. Continued use of our services indicates acceptance of the revised policy.",
-  },
+  // {
+  //   title: "3. How We Use Your Information",
+  //   description:
+  //     "Your information is used to provide services, process transactions, improve user experience, communicate updates, and maintain security.",
+  // },
+  // {
+  //   title: "4. Cookies & Tracking Technologies",
+  //   description:
+  //     "We use cookies and similar technologies to enhance functionality, analyze traffic, and personalize content.",
+  // },
+  // {
+  //   title: "5. Data Sharing",
+  //   description:
+  //     "We do not sell your personal data. Information may be shared with trusted partners or service providers strictly for operational purposes.",
+  // },
+  // {
+  //   title: "6. Data Security",
+  //   description:
+  //     "We implement industry-standard security measures to protect your data from unauthorized access, misuse, or disclosure.",
+  // },
+  // {
+  //   title: "7. Data Retention",
+  //   description:
+  //     "We retain personal information only as long as necessary to fulfill the purposes outlined in this policy or as required by law.",
+  // },
+  // {
+  //   title: "8. User Rights",
+  //   description:
+  //     "You have the right to access, update, correct, or delete your personal data and to opt out of marketing communications.",
+  // },
+  // {
+  //   title: "9. Third-Party Links",
+  //   description:
+  //     "Our platform may contain links to third-party websites. We are not responsible for their privacy practices or content.",
+  // },
+  // {
+  //   title: "10. Policy Updates",
+  //   description:
+  //     "We may update this Privacy Policy from time to time. Continued use of our services indicates acceptance of the revised policy.",
+  // },
 ];
 
 const PrivacyPolicy = () => {
